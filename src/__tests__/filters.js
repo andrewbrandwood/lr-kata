7e5c4eab02d4c1e1cadd0db30bc0783934ee9c55
andrewbrandwood <andrew@brandwood.com>
2019-05-21 08:43:31 +0100

    tests: extracting duplicates function to app

--- src/__tests__/filters.js
+++ src/__tests__/filters.js
@@ -1,11 +1,8 @@
+import { removeDuplicates } from '../common/functions';
+
 describe('when given an array', () =>{
   it('does not return duplicates', () => {
     const arr = ['test', 'test'];
-
-    const removeDuplicates = (arr) => {
-      return Array.from(new Set(arr));
-    }
-
     const newArr = removeDuplicates(arr);
     expect(newArr).toHaveLength(1);
   });
---
+++ src/common/functions.js
@@ -0,0 +1,3 @@
+export function removeDuplicates(arr){
+  return Array.from(new Set(arr));
+}
